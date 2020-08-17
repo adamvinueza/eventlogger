7d84315777cc2fc3dd836eb0e84e967620b3422c
Adam Vinueza <avinueza@strivehealth.com>
2020-08-16 18:20:07 -0600

    more tests

--- tests/test_client.py
+++ tests/test_client.py
@@ -8,3 +8,15 @@ class TestClient(unittest.TestCase):
     def test_init(self):
         c = logclient.LogClient()
         self.assertIsNotNone(c)
+
+    def test_add_field(self):
+        fld = {'label': True}
+        c = logclient.LogClient()
+        c.add_field('label', True)
+        self.assertEqual(c.fields._data, fld)
+
+    def test_add(self):
+        fld = {'label': True}
+        c = logclient.LogClient()
+        c.add(fld)
+        self.assertEqual(c.fields._data, fld)
