1fa78e23176f9d3eebc8468ddb4f6cb36db9ff8c
adamvinueza <adamvinueza@pm.me>
2020-12-13 17:36:48 -0700

    bump-version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(os.path.join(current_dir, 'README.md')) as rdr:
     long_description = rdr.read()
 
 setup(name='libevent',
-      version='0.3.0',
+      version='0.3.1',
       description='Library for sending events',
       long_description=long_description,
       url='http://github.com/adamvinueza/libevent',
